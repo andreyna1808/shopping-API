8238eedbbc6d2e4d648a5ad7d0852a612be324e6
Andreyna <andreyna.m.carvalho@gmail.com>
2022-07-30 22:18:13 -0400

    information for deploy

---
+++ src/deploy.ts
@@ -0,0 +1,8 @@
+/*
+// Separar em duas branchs (main(para o heroku) e develop(testes locais))
+// Tirar como exemplo o ormconfig, babel, e os script do package.json
+// Lembrando Typeorm fica nas dependências do projeto e não DevDependências
+// Criar uma aplicação no heroku e seguir os comandos de heroku login...
+// Rodar o heroku addons:create heroku-postgresql:hobby-dev
+// heroku run npx typeorm migration:run (esperar o sucesso sqn)
+*/
